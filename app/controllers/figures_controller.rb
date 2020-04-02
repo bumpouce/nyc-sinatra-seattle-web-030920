219fdd4b7812126ad3d6610038fc65228e21cbf5
Roger Robertson Jr <rjrjr@pm.me>
2020-04-02 16:18:40 -0700

    painful

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,15 +1,47 @@
 class FiguresController < ApplicationController
   # add controller methods
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+
   get '/figures/new' do
     @titles = Title.all 
     @landmarks = Landmark.all
     erb :'/figures/new'
   end 
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    #binding.pry
+    erb :'figures/show'
+  end
+
   post '/figures' do
-    binding.pry
-    @figure = Figure.create(params)
+    @figure = Figure.create(params[:figure])
+    #binding.pry
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+    else
+      @title = params[:title]
+    end
+    if !params[:landmark][:name].empty?
+      @lm = Landmark.create(params[:landmark])
+    else
+      @lm = params[:landmark]
+    end
+    #binding.pry
+    @lm.figure_id = @figure.id
+    @lm.save
+    FigureTitle.create(figure_id: @figure.id, title_id: @title.id)
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :edit
   end
   
 end
